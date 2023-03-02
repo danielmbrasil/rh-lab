45f6f19f3f1e6bae86ac4ebda291d44a2f427076
Daniel M Brasil <danielmbrasil@protonmail.com>
2023-03-02 10:42:42 -0300

    chore: add seeds for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 # Employee
-20.times { Employee.create(name: Faker::Name.name) }
+Employee.create(
+  [
+    {
+      name: Faker::Name.name
+    },
+    {
+      name: Faker::Name.name
+    },
+    {
+      name: Faker::Name.name
+    }
+  ]
+)
 
 # Department
 Department.create(
@@ -13,10 +25,6 @@ Department.create(
     {
       name: Faker::Lorem.sentence(word_count: 3),
       manager: Employee.first
-    },
-    {
-      name: Faker::Lorem.sentence(word_count: 3),
-      manager: Employee.last
     }
   ]
 )
@@ -43,56 +51,47 @@ Project.create(
   ]
 )
 
-# Position
-3.times { Position.create(name: Faker::Job.position) }
-
-# Assignment
-(2..5).each do |id|
-  Assignment.create(
-    starts_on: Time.zone.today,
-    employee: Employee.find(id),
-    project: Project.first
-  )
-end
-
-(5..10).each do |id|
-  Assignment.create(
-    starts_on: Time.zone.today,
-    employee: Employee.find(id),
-    project: Project.last
-  )
-end
-
 # Employment
 Employment.create(
   [
     {
-      salary: rand(0..10_000),
-      registration: Faker::Job.employment_type,
-      starts_on: 2.years.ago,
+      salary: rand(0..5_000),
+      registration: rand(1..1_000).to_s,
+      starts_on: 6.months.ago,
       reason: :hire,
-      department: Department.first,
-      employee: Employee.first,
-      position: Position.create(name: 'Manager')
+      department: Department.last,
+      employee: Employee.second,
+      position: Position.create(name: Faker::Job.position)
     },
     {
-      salary: rand(0..15_000),
-      registration: Faker::Job.employment_type,
-      starts_on: 6.months.ago,
-      reason: :promote,
+      salary: rand(0..1_000),
+      registration: rand(1..1_000).to_s,
+      starts_on: Time.zone.today,
       department: Department.last,
       employee: Employee.last,
-      position: Position.create(name: 'Manager')
+      position: Position.create(name: Faker::Job.position)
+    }
+  ]
+)
+
+# Assignment
+Assignment.create(
+  [
+    {
+      employee: Employee.second,
+      project: Project.first,
+      starts_on: 6.months.ago,
+      ends_on: 5.months.ago
     },
     {
-      salary: rand(0..5_000),
-      registration: Faker::Job.employment_type,
-      starts_on: 6.months.ago,
-      ends_on: Time.zone.today,
-      reason: :hire,
-      department: Department.last,
-      employee: Employee.find(15),
-      position: Position.create(name: Faker::Job.position)
+      employee: Employee.second,
+      project: Project.second,
+      starts_on: 4.months.ago
+    },
+    {
+      employee: Employee.last,
+      project: Project.last,
+      starts_on: Time.zone.tomorrow
     }
   ]
 )
