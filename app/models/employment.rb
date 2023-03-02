07050257aeda8cfa19922a13e4e6ecd8f3a9776a
Daniel M Brasil <danielmbrasil@protonmail.com>
2023-03-02 10:44:02 -0300

    feat: add scope to query active employments

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -18,9 +18,11 @@ class Employment < ApplicationRecord
   enum :reason, { hire: 1, promote: 10, restructure: 20 },
        default: :hire
 
+  scope :active, -> { where(ends_on: nil) }
+
   def only_one_active_employment
     return if ends_on.present?
-    return unless Employment.where(ends_on: nil, employee_id: id).exists?
+    return unless Employment.exists?(ends_on: nil, employee_id:)
 
     errors.add(:ends_on, "can't be blank if there's already an active employment")
   end
